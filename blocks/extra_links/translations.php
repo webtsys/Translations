24c769a7e52eb5f1636c398217ec7b06eee2c277
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-07-29 03:55:16 +0200

    Little fix in links for blocks

--- blocks/extra_links/translations.php
+++ blocks/extra_links/translations.php
@@ -5,8 +5,12 @@ global $lang;
 load_lang('translations');
 
 $select_page[]=$lang['translations']['translations'];
+$select_page[]='optgroup';
+
+$select_page[]=$lang['translations']['translations'];
 $select_page[]=make_fancy_url($base_url, 'translations', 'index', 'translations', $arr_data=array());
 
-
+$select_page[]=''; 
+$select_page[]='end_optgroup';
 
 ?>

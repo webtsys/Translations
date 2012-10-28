e46d1d5060f14fef78ef3478e802c80efcfd83a3
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-10-28 02:18:47 +0100

    Little fixes with SerializeField for save translations

--- i18n/en-US/translations.php
+++ i18n/en-US/translations.php
@@ -22,12 +22,6 @@ $lang['translations']['translation']='Translation';
 
 $lang['translations']['common_module']='common_module';
 
-$lang['translations']['error_cannot_mkdir']='Error: cannot mkdir';
-
-$lang['translations']['write_in_lang_file']='Write in language file';
-
-$lang['translations']['error_cannot_write_file']='Error: cannot write in the file';
-
 $lang['translations']['you_need_phardata_class']='You need the PharData class';
 
 $lang['translations']['error_cannot_write_translations_tar']='Error: cannot write translations package';
@@ -37,5 +31,11 @@ $lang['translations']['if_error_you_have_put_permissions']='If you have error, y
 $lang['translations']['download_lang_file']='Download language files';
 
 $lang['translations']['no_exists_i18n_here']='Language file does not found';
+
+$lang['translations']['error_cannot_mkdir']='Error: cannot mkdir';
+
+$lang['translations']['write_in_lang_file']='Write in language file';
+
+$lang['translations']['error_cannot_write_file']='Error: cannot write in the file';
 
 ?>

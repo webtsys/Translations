85d7b53d4f081ab1739a3c54b9404c46f1c33aaf
Antonio de la Rosa <webmaster@web-t-sys.com>
2012-11-01 22:21:37 +0100

    Complete rewrite i18n

--- i18n/es-ES/translations.php
+++ i18n/es-ES/translations.php
@@ -22,12 +22,6 @@ $lang['translations']['translation']='Traducción';
 
 $lang['translations']['common_module']='common_module';
 
-$lang['translations']['error_cannot_mkdir']='Error: no puedo ejecutr mkdir';
-
-$lang['translations']['write_in_lang_file']='Escribiendo en fichero de lenguaje';
-
-$lang['translations']['error_cannot_write_file']='Error: no puedo escribir en el fichero';
-
 $lang['translations']['you_need_phardata_class']='Este módulo necesita la clase PharData';
 
 $lang['translations']['error_cannot_write_translations_tar']='Error: no puedo escribir paquete de traducciones';
@@ -37,5 +31,11 @@ $lang['translations']['if_error_you_have_put_permissions']='Si tu tienes un erro
 $lang['translations']['download_lang_file']='Descargar ficheros de lenguaje';
 
 $lang['translations']['no_exists_i18n_here']='El fichero de lenguaje no fue encontrado';
+
+$lang['translations']['error_cannot_mkdir']='Error: no puedo ejecutr mkdir';
+
+$lang['translations']['write_in_lang_file']='Escribiendo en fichero de lenguaje';
+
+$lang['translations']['error_cannot_write_file']='Error: no puedo escribir en el fichero';
 
 ?>
